b4ad51d03124a8b6ee2c9c8989cfc1325c6a7dd0
Victor Gomes <104176261+victorgomesdev@users.noreply.github.com>
2024-06-21 17:14:25 -0300

    Função de listar os robos

--- src/services/devices/index.ts
+++ src/services/devices/index.ts
@@ -1,3 +1,24 @@
+import {Alert} from 'react-native'
 export function cadastrar(){
     
+}
+
+export async function listDevices(ownerid: string, token: string){
+    try{
+        const result = await fetch('http://31.220.81.141:3000', {
+            method: 'post',
+            headers:{
+                'Content-Type':'application/json' 
+            },
+            body: JSON.stringfy({
+                token: token,
+                ownerId: ownerId
+            })
+        })
+
+        const devices = await result.json()
+        return devices
+    }catch(e){
+        Alert.alert('Erro', 'Verifique a sua conexão com a internet')
+    }
 }
